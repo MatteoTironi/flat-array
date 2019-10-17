ad888158f4f12d0ff84cddbf3a9b75e6723861fb
MatteoTironi <matt.tironi@gmail.com>
2019-10-17 18:00:30 +0200

    flatArray recursive done

--- index.js
+++ index.js
@@ -22,28 +22,47 @@ Write tests using `Jest`
  */
 
 function flatArray(array, res = []) {
-    for (let i = 0; i < array.length; i++) {
-        //il valore a quella posizione è un array?
-        if (Array.isArray(array[i])) {
-            //l'array è maggiore di uno?
-            if (array[i].length > 1) {
-                let subArray = []
-                for (let j = 0; j < array.length; j++) {
-                    subArray = [...array[i]]
-                }
-                res.push(...subArray);
-            }
-            res = [...res, ...array[i]]
-        }
-        if (!Array.isArray(array[i]))
-            res = [...res, array[i]]
+    if (array === undefined || array === null || typeof array === "string") {
+        return res = [];
     }
-    return res;
+    else {
+        for (let i = 0; i < array.length; i++) {
+            //il valore alla posizione i è un array?
+            if (Array.isArray(array[i])) {
+                flatArray(array[i], res)
+            }
+            else {
+                res.push(array[i])
+            }
+        }
+        return res;
+    }
 
 }
-flatArray([1, [29], [67, [7, [57], 5]]])
 
-//[67,[7 ,[57] ,5]]
+flattingArray([1, 2, [5, [7]], 5])
+
+function flatArray(array) {
+
+    for (var i = 0; i < array.length; i++) {
+
+        if (array[i] instanceof Array) {
+
+            Array.prototype.splice.apply(array, [i, 1].concat(array[i]))
+            i--;
+        }
+
+    }
+
+    return array;
+}
+
+
+/**
+ * Per funzionare correttamente, la funzione ricorsiva deve ridurre ai minimi termini i valori in ingresso, nell'esempio sotto 
+ * l'array è stata ridotta al singolo valore 
+ * 
+ */
 
 function sum(arr, res = 0) {
     if (arr.length > 0) {
